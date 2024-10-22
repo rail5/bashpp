39b449cdeced58557f448eaf41db8b50c3200275
rail5 <andrew@rail5.org>
2024-10-22 11:35:18 +0800

    Bash++ is a much better name than Object Bash

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
-# Object Bash
+# Bash++
 
 Bash with classes
+
+Copright (C) 2024 rail5
+
+## The Basic Idea
+
+A Bash++ script will be read first by the Bash++ interpreter.
+The interpreter parses the script, identifies the objects and their methods
+At the end, the interpreter will generate an ordinary procedural Bash script, and the generated script will be passed to Bash proper for execution
+
+## Syntax
+
+The Bash++ script will be written in a special syntax
+The syntax will be a superset of the Bash syntax
+The syntax will be designed to be easily parsed by the Bash++ interpreter
+The syntax will be designed to be easily converted to ordinary Bash script
+The syntax will be designed to be easily understood by the programmer
+The syntax will be designed to be easily written by the programmer
+
+Here is the basic syntax for class definition:
+
+```
+@class ClassName {
+
+   @property [public|private|protected] propertyName
+
+   @constructor [parameters] {
+	  	constructor body
+   }
+
+   @method [public|private|protected] methodName {
+	  	method body
+   }
+}
+```
---
+++ main.cpp
@@ -0,0 +1,92 @@
+/**
+ * Bash++: The Bourne-Again Shell with Classes
+ * Copyright (C) 2024 rail5
+ */
+
+/**
+ * The basic idea here:
+ * A Bash++ script will be read first by the Bash++ interpreter.
+ * The interpreter parses the script, identifies the objects and their methods
+ * At the end, the interpreter will generate an ordinary procedural Bash script
+ * And the generated script will be passed to Bash proper for execution
+ */
+
+/**
+ * The Bash++ script will be written in a special syntax
+ * The syntax will be a superset of the Bash syntax
+ * The syntax will be designed to be easily parsed by the Bash++ interpreter
+ * The syntax will be designed to be easily converted to ordinary Bash script
+ * The syntax will be designed to be easily understood by the programmer
+ * The syntax will be designed to be easily written by the programmer
+ * 
+ * Here is the basic syntax for class definition:
+ * 
+ * @class ClassName {
+ * 
+ *    @property [public|private|protected] propertyName
+ * 
+ *    @constructor [parameters] {
+ * 	  	constructor body
+ *    }
+ * 
+ *    @method [public|private|protected] methodName {
+ * 	  	method body
+ *    }
+ * }
+ * 
+ * Here is the basic syntax for object creation:
+ * @object objectName = new ClassName
+ * 
+ * Here is the basic syntax for method invocation:
+ * objectName.methodName
+ * 
+ * Here is the basic syntax for property access:
+ * objectName.propertyName
+ * 
+ * Here is the basic syntax for method invocation with parameters:
+ * objectName.methodName parameter1 parameter2 parameter3
+ * 
+ * Here is the basic syntax for property assignment:
+ * objectName.propertyName = value
+ * 
+ * Here is the basic syntax for property access with assignment:
+ * value = objectName.propertyName
+ * 
+ * Here is the basic syntax for method invocation with parameters and assignment:
+ * value = objectName.methodName parameter1 parameter2 parameter3
+ * 
+ * With all of this, we can write Bash lines like the following:
+ * if [[ objectName.methodName parameter1 parameter2 parameter3 -eq 0 ]]; then
+ * 	   echo "The method returned 0"
+ * elif [[ objectName.propertyName == "value" ]]; then
+ * 	   echo "The property is set to value"
+ * fi
+ * 
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+		return 1;
+	}
+	
+	std::ifstream file(argv[1]);
+	if (!file.is_open()) {
+		std::cerr << "Error: Could not open file " << argv[1] << std::endl;
+		return 1;
+	}
+	
+	std::string line;
+	while (std::getline(file, line)) {
+		std::cout << line << std::endl;
+	}
+	
+	file.close();
+	
+	return 0;
+}

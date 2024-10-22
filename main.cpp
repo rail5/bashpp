e3e0b2c75689ad17e2a7689e072211dae9bcf484
rail5 <andrew@rail5.org>
2024-10-22 21:24:27 +0800

    Removed comment that belongs in readme

--- main.cpp
+++ main.cpp
@@ -3,67 +3,6 @@
  * Copyright (C) 2024 rail5
  */
 
-/**
- * The basic idea here:
- * A Bash++ script will be read first by the Bash++ interpreter.
- * The interpreter parses the script, identifies the objects and their methods
- * At the end, the interpreter will generate an ordinary procedural Bash script
- * And the generated script will be passed to Bash proper for execution
- */
-
-/**
- * The Bash++ script will be written in a special syntax
- * The syntax will be a superset of the Bash syntax
- * The syntax will be designed to be easily parsed by the Bash++ interpreter
- * The syntax will be designed to be easily converted to ordinary Bash script
- * The syntax will be designed to be easily understood by the programmer
- * The syntax will be designed to be easily written by the programmer
- * 
- * Here is the basic syntax for class definition:
- * 
- * @class ClassName {
- * 
- *    @property [public|private|protected] propertyName
- * 
- *    @constructor [parameters] {
- * 	  	constructor body
- *    }
- * 
- *    @method [public|private|protected] methodName {
- * 	  	method body
- *    }
- * }
- * 
- * Here is the basic syntax for object creation:
- * @object objectName = new ClassName
- * 
- * Here is the basic syntax for method invocation:
- * @objectName.methodName
- * 
- * Here is the basic syntax for property access:
- * @objectName.propertyName
- * 
- * Here is the basic syntax for method invocation with parameters:
- * @objectName.methodName parameter1 parameter2 parameter3
- * 
- * Here is the basic syntax for property assignment:
- * @objectName.propertyName = value
- * 
- * Here is the basic syntax for property access with assignment:
- * value = @objectName.propertyName
- * 
- * Here is the basic syntax for method invocation with parameters and assignment:
- * value = @objectName.methodName parameter1 parameter2 parameter3
- * 
- * With all of this, we can write Bash lines like the following:
- * if [[ @objectName.methodName parameter1 parameter2 parameter3 -eq 0 ]]; then
- * 	   echo "The method returned 0"
- * elif [[ @objectName.propertyName == "value" ]]; then
- * 	   echo "The property value is: @objectName.propertyName"
- * fi
- * 
- */
-
 #include <iostream>
 #include <string>
 #include <vector>

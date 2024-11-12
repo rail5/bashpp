7b9ea79c1e4efb3eba1ce3f3219155e157a78d4c
rail5 <andrew@rail5.org>
2024-11-12 13:48:43 +0800

    Some small progress on using the parser

--- antlr/bpp_class.cpp
+++ antlr/bpp_class.cpp
@@ -1,10 +1,102 @@
+#include <ostream>
 #include <string>
 #include <vector>
 #include <map>
 
-enum BPP_SCOPE {
+enum BPP_SCOPE_ENUM {
 	SCOPE_PUBLIC,
-	SCOPE_PRIVATE
+	SCOPE_PRIVATE,
+	SCOPE_UNDEFINED
+};
+
+class BPP_SCOPE {
+	private:
+		BPP_SCOPE_ENUM scope;
+	public:
+		BPP_SCOPE() {
+			scope = SCOPE_PRIVATE;
+		}
+
+		explicit BPP_SCOPE(BPP_SCOPE_ENUM scope) {
+			this->scope = scope;
+		}
+
+		explicit BPP_SCOPE(std::string scope) {
+			if (scope == "@public") {
+				this->scope = SCOPE_PUBLIC;
+			} else {
+				this->scope = SCOPE_PRIVATE;
+			}
+		}
+
+		BPP_SCOPE_ENUM getScope() const {
+			return scope;
+		}
+
+		std::string toString() const {
+			if (scope == SCOPE_PUBLIC) {
+				return "@public";
+			} else {
+				return "@private";
+			}
+		}
+
+		friend std::ostream& operator<<(std::ostream& os, const BPP_SCOPE& scope) {
+			os << scope.toString();
+			return os;
+		}
+
+		bool operator==(BPP_SCOPE_ENUM scope) {
+			return this->scope == scope;
+		}
+
+		bool operator!=(BPP_SCOPE_ENUM scope) {
+			return this->scope != scope;
+		}
+
+		bool operator==(BPP_SCOPE scope) {
+			return this->scope == scope.getScope();
+		}
+
+		bool operator!=(BPP_SCOPE scope) {
+			return this->scope != scope.getScope();
+		}
+
+		bool operator==(std::string scope) {
+			return this->toString() == scope;
+		}
+
+		bool operator!=(std::string scope) {
+			return this->toString() != scope;
+		}
+
+		BPP_SCOPE operator=(BPP_SCOPE_ENUM scope) {
+			this->scope = scope;
+			return *this;
+		}
+
+		BPP_SCOPE operator=(BPP_SCOPE scope) {
+			this->scope = scope.getScope();
+			return *this;
+		}
+
+		BPP_SCOPE operator=(const char* scope) {
+			if (scope == "@public") {
+				this->scope = SCOPE_PUBLIC;
+			} else {
+				this->scope = SCOPE_PRIVATE;
+			}
+			return *this;
+		}
+
+		BPP_SCOPE operator=(const std::string& scope) {
+			if (scope == "@public") {
+				this->scope = SCOPE_PUBLIC;
+			} else {
+				this->scope = SCOPE_PRIVATE;
+			}
+			return *this;
+		}
 };
 
 struct bpp_class_variable {
@@ -16,6 +108,7 @@ struct bpp_class_variable {
 struct bpp_class_method {
 	std::string method_name;
 	std::string method_body;
+	std::vector<bpp_class_variable> method_parameters;
 	BPP_SCOPE method_scope;
 };
 
--- antlr/main.cpp
+++ antlr/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <set>
 #include <map>
 #include <antlr4-runtime.h>
 
@@ -10,39 +12,226 @@
 
 using namespace antlr4;
 
+std::set<std::string> protected_keywords = {  "public", "private", "class", "method", "constructor", "primitive" };
+std::set<std::string> used_symbols;
 std::map<std::string, bpp_class> classes;
-std::string current_class = "";
 int brace_depth = 0;
 
-std::string build_program(tree::ParseTree* tree) {
+#define SYMBOL_SEPARATOR "_"
+
+std::string generate_symbol(bool in_class, std::string class_name,
+		bool in_method, std::string method_name,
+		bool in_constructor,
+		std::vector<bpp_class_variable> parameters = {},
+		bool is_variable = false, std::string variable_name = "") {
+
+	std::string symbol = "";
+
+	#define MAYBE_SYMBOL_SEPARATOR (!symbol.empty() ? SYMBOL_SEPARATOR : "")
+	
+	if (in_class) {
+		symbol += class_name;
+	}
+
+	if (in_method) {
+		symbol += MAYBE_SYMBOL_SEPARATOR;
+		symbol += method_name;
+	}
+
+	if (in_constructor) {
+		symbol += MAYBE_SYMBOL_SEPARATOR;
+		symbol += "constructor";
+	}
+
+	if (!parameters.empty()) {
+		symbol += MAYBE_SYMBOL_SEPARATOR;
+		for (auto& parameter : parameters) {
+			symbol += MAYBE_SYMBOL_SEPARATOR;
+			symbol += parameter.variable_type;
+		}
+	}
+
+	if (is_variable) {
+		if (in_class) {
+			symbol += MAYBE_SYMBOL_SEPARATOR;
+			symbol += "${objectName}";
+		}
+		symbol += MAYBE_SYMBOL_SEPARATOR;
+		symbol += variable_name;
+	}
+
+	return symbol;
+}
+
+std::string generate_method_signature(ParserRuleContext* ruleContext, bool is_constructor = false) {
+	// The ruleContext is the method_definition or constructor_definition rule
+	std::string signature;
+	if (is_constructor) {
+		signature += "constructor";
+	} else {
+		signature += ruleContext->children[2]->getText();
+	}
+
+	// Iterate over the ruleContext until we find the parameter list
+	for (size_t i = 0; i < ruleContext->children.size(); i++) {
+		auto innerRuleContext = dynamic_cast<ParserRuleContext*>(ruleContext->children[i]);
+		if (innerRuleContext) {
+			if (innerRuleContext->getRuleIndex() == BashppParser::RuleParameter_list) {
+				// Found the parameter list
+				signature += SYMBOL_SEPARATOR;
+				bool type_set_without_name = false;
+				for (size_t j = 0; j < innerRuleContext->children.size(); j++) {
+					auto terminalNode = dynamic_cast<tree::TerminalNode*>(innerRuleContext->children[j]);
+					if (terminalNode) {
+						// Is this token a BASHPP_VARIABLE? Or an IDENTIFIER?
+						// We only care about the type
+						switch (terminalNode->getSymbol()->getType()) {
+							case BashppLexer::BASHPP_VARIABLE:
+								signature += SYMBOL_SEPARATOR;
+								signature += terminalNode->getText().substr(1, terminalNode->getText().size() - 1); // Strip the @ symbol
+								type_set_without_name = true;
+								break;
+							case BashppLexer::IDENTIFIER:
+								if (type_set_without_name) {
+									// Name found, not primitive, move along
+									type_set_without_name = false;
+								} else {
+									// Primitive type
+									signature += SYMBOL_SEPARATOR;
+									signature += "primitive";
+								}
+								break;
+							default:
+								break;
+						}
+					}
+				}
+				break;
+			}
+		}
+	}
+
+	return signature;
+}
+
+std::string build_program(tree::ParseTree* tree,
+		bool in_class = false, std::string class_name = "",
+		bool in_method = false, std::string method_signature = "",
+		bool in_constructor = false) {
 	std::string program;
 	for (size_t i = 0; i < tree->children.size(); i++) {
 		auto ruleContext = dynamic_cast<ParserRuleContext*>(tree->children[i]);
 		if (ruleContext) {
 			switch (ruleContext->getRuleIndex()) {
 				case BashppParser::RuleProgram:
-					program += "#!/usr/bin/env bash\n";
-					program += build_program(tree->children[i]);
+					program += build_program(tree->children[i], in_class, class_name, in_method, method_signature, in_constructor);
 					break;
 				case BashppParser::RuleStatement:
 					std::cout << "Descending into statement: " << tree->children[i]->getText() << std::endl;
-					program += build_program(tree->children[i]);
+					program += build_program(tree->children[i], in_class, class_name, in_method, method_signature, in_constructor);
 					break;
-				case BashppParser::RuleClass_definition:
-					current_class = ruleContext->children[1]->getText();
-					classes[current_class] = bpp_class();
-					std::cout << "Class definition: " << current_class << std::endl;
-					break;
+				case BashppParser::RuleClass_definition: {
+					if (in_class) {
+						throw std::runtime_error("Cannot define a class within a class");
+					}
+
+					class_name = ruleContext->children[1]->getText();
+
+					std::string class_symbol = generate_symbol(true, class_name, in_method, method_signature, in_constructor);
+					std::cout << "Generated symbol for class: " << class_symbol << std::endl;
+
+					if (used_symbols.find(class_symbol) != used_symbols.end()) {
+						throw std::runtime_error("Symbol '" + class_symbol + "' already in use");
+					}
+
+					used_symbols.insert(class_symbol);
+
+					if (protected_keywords.find(class_name) != protected_keywords.end()) {
+						throw std::runtime_error("Cannot use keyword '" + class_name + "' as a class name");
+					}
+
+					classes[class_name] = bpp_class();
+					std::cout << "Class definition: " << class_name << std::endl;
+					in_class = true;
+					program += build_program(tree->children[i], in_class, class_name, in_method, method_signature, in_constructor);
+					break; }
 				case BashppParser::RuleClass_body_statement: {
-					auto innerRuleContext = dynamic_cast<ParserRuleContext*>(ruleContext->children[1]);
+					if (!in_class) {
+						throw std::runtime_error("Cannot define a class body statement outside of a class");
+					}
+					std::cout << "Trying to parse class_body_statement: " << ruleContext->children[0]->getText() << std::endl;
+					auto innerRuleContext = dynamic_cast<ParserRuleContext*>(ruleContext->children[0]);
 					if (innerRuleContext) {
 						switch (innerRuleContext->getRuleIndex()) {
-							case BashppParser::RuleVariable_definition:
+							case BashppParser::RuleVariable_definition: {
 								std::cout << "Variable definition: " << innerRuleContext->children[0]->getText() << std::endl;
-								break;
-							case BashppParser::RuleMethod_definition:
+								size_t number_of_tokens = innerRuleContext->children.size();
+								std::string variable_name, variable_type, variable_scope;
+								variable_scope = innerRuleContext->children[0]->getText();
+								if (number_of_tokens == 3) {
+									variable_name = innerRuleContext->children[2]->getText();
+									variable_type = innerRuleContext->children[1]->getText();
+								} else {
+									variable_name = innerRuleContext->children[1]->getText();
+									variable_type = ""; // Primitive type
+								}
+
+								if (protected_keywords.find(variable_name) != protected_keywords.end()) {
+									throw std::runtime_error("Cannot use keyword '" + variable_name + "' as a variable name");
+								}
+
+								if (classes[class_name].variables.find(variable_name) != classes[class_name].variables.end()) {
+									throw std::runtime_error("Variable '" + variable_name + "' already defined in class '" + class_name + "'");
+								}
+
+								std::string variable_symbol = generate_symbol(in_class, class_name, in_method, method_signature, in_constructor, {}, true, variable_name);
+
+								if (used_symbols.find(variable_symbol) != used_symbols.end()) {
+									throw std::runtime_error("Symbol '" + variable_symbol + "' already in use");
+								}
+
+								std::cout << "Generated symbol for variable: " << variable_symbol << std::endl;
+
+								used_symbols.insert(variable_symbol);
+
+								classes[class_name].variables[variable_name] = bpp_class_variable();
+								classes[class_name].variables[variable_name].variable_type = variable_type;
+								classes[class_name].variables[variable_name].variable_name = variable_name;
+								classes[class_name].variables[variable_name].variable_scope = variable_scope;
+								std::cout << "Declared type: " << classes[class_name].variables[variable_name].variable_type << std::endl;
+								std::cout << "Declared scope: " << classes[class_name].variables[variable_name].variable_scope << std::endl;
+								std::cout << "Declared name: " << classes[class_name].variables[variable_name].variable_name << std::endl;
+								break; }
+							case BashppParser::RuleMethod_definition: {
 								std::cout << "Method definition: " << innerRuleContext->children[0]->getText() << std::endl;
-								break;
+								in_method = true;
+								std::string method_name = innerRuleContext->children[2]->getText();
+
+								if (protected_keywords.find(method_name) != protected_keywords.end()) {
+									throw std::runtime_error("Cannot use keyword '" + method_name + "' as a method name");
+								}
+
+								method_signature = generate_method_signature(innerRuleContext);
+								std::cout << "Generated method signature: " << method_signature << std::endl;
+
+								if (classes[class_name].methods.find(method_signature) != classes[class_name].methods.end()) {
+									throw std::runtime_error("Method signature '" + method_signature + "' already defined in class '" + class_name + "'");
+								}
+
+								std::string method_symbol = generate_symbol(in_class, class_name, in_method, method_signature, in_constructor);
+								std::cout << "Generated method symbol: " << method_symbol << std::endl;
+								if (used_symbols.find(method_symbol) != used_symbols.end()) {
+									throw std::runtime_error("Symbol '" + method_symbol + "' already in use");
+								}
+								used_symbols.insert(method_symbol);
+
+								classes[class_name].methods[method_signature] = bpp_class_method();
+								classes[class_name].methods[method_signature].method_name = method_name;
+								classes[class_name].methods[method_signature].method_scope = innerRuleContext->children[0]->getText();
+								std::cout << "Declared scope: " << classes[class_name].methods[method_signature].method_scope << std::endl;
+								std::cout << "Declared name: " << classes[class_name].methods[method_signature].method_name << std::endl;
+								program += build_program(innerRuleContext, in_class, class_name, in_method, method_name, in_constructor);
+								break; }
 							case BashppParser::RuleConstructor_definition:
 								std::cout << "Constructor definition: " << innerRuleContext->children[0]->getText() << std::endl;
 								break;
@@ -53,13 +242,76 @@ std::string build_program(tree::ParseTree* tree) {
 					}
 					break;
 				}
+				case BashppParser::RuleParameter_list:
+					if (!in_method && !in_constructor) {
+						throw std::runtime_error("Cannot define a parameter list outside of a method or constructor");
+					}
+					std::cout << "Parameter list: " << ruleContext->getText() << std::endl;
+					// Iterate over each token in the parameter list
+					for (size_t i = 0; i < ruleContext->children.size(); i++) {
+						// Is this token a BASHPP_VARIABLE? Or an IDENTIFIER?
+						auto terminalNode = dynamic_cast<tree::TerminalNode*>(ruleContext->children[i]);
+						if (terminalNode) {
+							switch (terminalNode->getSymbol()->getType()) {
+								case BashppLexer::BASHPP_VARIABLE:
+									std::cout << "BASHPP_VARIABLE: " << terminalNode->getText() << std::endl;
+									classes[class_name].methods[method_signature].method_parameters.push_back(bpp_class_variable());
+									classes[class_name].methods[method_signature].method_parameters.back().variable_type = terminalNode->getText();
+									break;
+								case BashppLexer::IDENTIFIER:
+									std::cout << "IDENTIFIER: " << terminalNode->getText() << std::endl;
+									if (classes[class_name].methods[method_signature].method_parameters.size() > 0) {
+										// Does the last-pushed variable have a name?
+										if (classes[class_name].methods[method_signature].method_parameters.back().variable_name.empty()) {
+											// No name -- that means we pushed a type, and this is the name
+											classes[class_name].methods[method_signature].method_parameters.back().variable_name = terminalNode->getText();
+											classes[class_name].methods[method_signature].method_parameters.back().variable_scope = SCOPE_UNDEFINED;
+										} else {
+											// There was a name. That means this is a new variable, and it's a primitive type
+											classes[class_name].methods[method_signature].method_parameters.push_back(bpp_class_variable());
+											classes[class_name].methods[method_signature].method_parameters.back().variable_type = "";
+											classes[class_name].methods[method_signature].method_parameters.back().variable_name = terminalNode->getText();
+											classes[class_name].methods[method_signature].method_parameters.back().variable_scope = SCOPE_UNDEFINED;
+										}
+									} else {
+										// No parameters yet, so this is a primitive type
+										classes[class_name].methods[method_signature].method_parameters.push_back(bpp_class_variable());
+										classes[class_name].methods[method_signature].method_parameters.back().variable_type = "";
+										classes[class_name].methods[method_signature].method_parameters.back().variable_name = terminalNode->getText();
+										classes[class_name].methods[method_signature].method_parameters.back().variable_scope = SCOPE_UNDEFINED;
+									}
+
+									std::cout << "Declared type: " << classes[class_name].methods[method_signature].method_parameters.back().variable_type << std::endl;
+									std::cout << "Declared scope: " << classes[class_name].methods[method_signature].method_parameters.back().variable_scope << std::endl;
+									std::cout << "Declared name: " << classes[class_name].methods[method_signature].method_parameters.back().variable_name << std::endl;
+
+									break;
+								default:
+									std::cout << "Not identified: " << terminalNode->getSymbol()->getType() << std::endl;
+									break;
+							}
+						}
+					}
+					break;
+				case BashppParser::RuleOther_statement:
+					std::cout << "Other statement: " << ruleContext->getText() << std::endl;
+					if (in_method) {
+						std::cout << "Added statement to method body for '" << method_signature << "'" << std::endl;
+						classes[class_name].methods[method_signature].method_body += ruleContext->getText();
+					} else if (in_constructor) {
+						classes[class_name].constructors.back().constructor_body += ruleContext->getText();
+					} else {
+						program += ruleContext->getText();
+					}
+					break;
 				case BashppParser::RuleOpen_brace:
 					brace_depth++;
 					break;
 				case BashppParser::RuleClose_brace:
 					brace_depth--;
 					if (brace_depth == 0) {
-						current_class = "";
+						class_name = "";
+						in_class = false;
 					}
 					break;
 				default:

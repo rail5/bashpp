5afa9891d97b327ce235f914dfc7e00a7b67ef54
rail5 <andrew@rail5.org>
2024-12-27 19:40:32 +0800

    Added supershell example and hand-compilation

---
+++ scripts/supershell-compiled.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+function bpp____supershell() {
+	local outputVar="$1" command="$2"
+	local tempFile=$(mktemp -p /dev/shm/)
+	exec {bpp__supershellFD}<>"$tempFile"
+	rm "$tempFile"
+	$command 1>&${bpp__supershellFD} 2>/dev/null
+	eval "$outputVar=\$(cat "/proc/self/fd/${bpp__supershellFD}")"
+	exec 3>&-
+}
+
+function bpp__MyClass____new() {
+	local objectName="$1" setPtr="$2"
+	
+	if [[ -z "$objectName" ]]; then
+		while : ; do
+			objectName="$RANDOM$RANDOM$RANDOM$RANDOM"
+			local inUse="bpp__MyClass__${objectName}____inUse"
+			[[ -z "${!inUse}" ]] && break
+		done
+	fi
+	local objectAddress="bpp__MyClass__${objectName}"
+	eval "${objectAddress}____inUse=1"
+	eval "${objectAddress}__dataMember=\"a\""
+	if [[ ! -z "$setPtr" ]]; then
+		eval "${setPtr}=\${objectAddress}"
+	fi
+}
+
+function bpp__MyClass____delete() {
+	local objectName="$1" objectIsPtr="$2"
+	local objectAddress="bpp__MyClass__${objectName}"
+	if [[ "${objectIsPtr}" -eq 1 ]]; then
+		objectAddress="${objectName}"
+	fi
+	unset "${objectAddress}__dataMember"
+	unset "${objectAddress}____inUse"
+}
+
+function bpp__MyClass____copy() {
+	local copyFrom="$1" copyTo="$2" copyFromIsPtr="$3" copyToIsPtr="$4"
+	local copyFromAddress="bpp__MyClass__${copyFrom}" copyToAddress="bpp__MyClass__${copyTo}"
+	if [[ "${copyFromIsPtr}" -eq 1 ]]; then
+		copyFromAddress="${copyFrom}"
+	fi
+	if [[ "${copyToIsPtr}" -eq 1 ]]; then
+		copyToAddress="${copyTo}"
+	fi
+	local copyFrom__dataMember="${copyFromAddress}__dataMember"
+	eval "${copyToAddress}__dataMember=\${!copyFrom__dataMember}"
+	eval "${copyToAddress}____inUse=1"
+}
+
+function bpp__MyClass__toPrimitive() {
+	echo "MyClass Instance"
+}
+
+function bpp__MyClass__set____primitive() {
+	local objectName="$1" objectIsPtr="$2" arg="$3"
+	local objectAddress="bpp__MyClass__${objectName}"
+	if [[ "${objectIsPtr}" -eq 1 ]]; then
+		objectAddress="${objectName}"
+	fi
+	local this__dataMember="${objectAddress}__dataMember"
+	if [[ "${!this__dataMember}" == "$arg" ]]; then
+		echo "dataMember is already set to $arg"
+	else
+		eval "${this__dataMember}=\$arg"
+		echo "dataMember was not already set to $arg"
+	fi
+}
+
+bpp__MyClass____new "myObject"
+bpp__MyClass__set____primitive "myObject" 0 "b"
+echo "$bpp__MyClass__myObject__dataMember"
+
+function __runfunc() {
+	bpp__MyClass__set____primitive "myObject" 0 "c"
+}
+bpp____supershell "var" __runfunc
+unset -f __runfunc
+echo "$var"
+echo "$bpp__MyClass__myObject__dataMember"

75bb9500631c2d7f02b207152ff1e14417a6f4e8
rail5 <andrew@rail5.org>
2025-02-05 14:26:53 +0800

    Remember to add newlines after exiting comments

--- src/listener/handlers/comment.cpp
+++ src/listener/handlers/comment.cpp
@@ -16,6 +16,14 @@ void BashppListener::enterComment(BashppParser::CommentContext *ctx) {
 void BashppListener::exitComment(BashppParser::CommentContext *ctx) {
 	skip_syntax_errors
 	in_comment = false;
+
+	// Get the current code entity
+	std::shared_ptr<bpp::bpp_code_entity> current_code_entity = std::dynamic_pointer_cast<bpp::bpp_code_entity>(entity_stack.top());
+
+	if (current_code_entity != nullptr) {
+		// Add a newline
+		current_code_entity->add_code("\n");
+	}
 }
 
 #endif // SRC_LISTENER_HANDLERS_COMMENT_CPP_

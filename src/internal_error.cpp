f9db3cd171e0e457406295be9d6021042891fe69
rail5 <andrew@rail5.org>
2025-03-19 17:29:58 +0800

    Doxygen documentation for internal_error

--- src/internal_error.cpp
+++ src/internal_error.cpp
@@ -8,6 +8,14 @@
 
 #include <stdexcept>
 
+/**
+ * @struct internal_error
+ * 
+ * @brief An exception thrown when an internal error occurs
+ * 
+ * Internal errors are errors which should never occur in normal operation.
+ * They are indicative of a bug in the Bash++ compiler, and halt compilation.
+ */
 struct internal_error : public std::runtime_error {
 	internal_error(const std::string& msg, antlr4::tree::ParseTree* location)
 		: std::runtime_error(msg + "\nYou've found a bug! Please report it.\nContext: " + location->getText()) {}

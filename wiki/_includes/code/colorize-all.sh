8b6b99a404e97ce17ec1cef5a23c87626897e836
rail5 <andrew@rail5.org>
2025-05-03 09:59:11 +0800

    Colorize code snippets 5 at a time

--- wiki/_includes/code/colorize-all.sh
+++ wiki/_includes/code/colorize-all.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+counter=0
+max_jobs=5
+
 for snippet in "./snippets/"*; do
 	outputFile=${snippet//.bpp/.html}
-	node colorize.js "$snippet" "$outputFile"
+	node colorize.js "$snippet" "$outputFile" &
+	((counter++))
+	if ((counter % max_jobs == 0)); then
+		wait
+	fi
 done
+
+wait

6884c6a389a1b1773d92dac0ca56c943cc488894
rail5 <andrew@rail5.org>
2025-07-11 13:53:38 +0800

    Language server: move helpers to separate translation unit

--- src/language-server/main.cpp
+++ src/language-server/main.cpp
@@ -13,93 +13,21 @@
 #include <thread>
 #include <csignal>
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/un.h>
-
 #include <ext/stdio_filebuf.h> // Non-portable GNU extension
 
 #include <unistd.h>
 #include <getopt.h>
 
 #include "BashppServer.h"
+#include "helpers.h"
 
 #include "../version.h"
 #include "../updated_year.h"
 
-static BashppServer server;
-static int client_fd = -1;
-static int server_fd = -1;
-static int socket_port = 0;
-
-void signal_handler(int signum) {
-	server.log("Received signal: ", signum, ", cleaning up and exiting.");
-	server.cleanup();
-	if (client_fd != -1) {
-		close(client_fd);
-	}
-	if (server_fd != -1) {
-		close(server_fd);
-	}
-	exit(signum);
-}
-
-int setup_tcp_server(int port) {
-	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd < 0) {
-		std::cerr << "Error creating socket" << std::endl;
-		return -1;
-	}
-
-	struct sockaddr_in address;
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(static_cast<uint16_t>(port));
-
-	if (bind(server_fd, reinterpret_cast<struct sockaddr*>(&address), sizeof(address)) < 0) {
-		std::cerr << "Error binding socket" << std::endl;
-		close(server_fd);
-		return -1;
-	}
-
-	if (listen(server_fd, 3) < 0) {
-		std::cerr << "Error listening on socket" << std::endl;
-		close(server_fd);
-		return -1;
-	}
-
-	return server_fd;
-}
-
-int setup_unix_socket_server(const std::string& path) {
-	int server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-	if (server_fd < 0) {
-		std::cerr << "Error creating Unix socket" << std::endl;
-		return -1;
-	}
-
-	struct sockaddr_un address;
-	address.sun_family = AF_UNIX;
-	strncpy(address.sun_path, path.c_str(), sizeof(address.sun_path) - 1);
-	address.sun_path[sizeof(address.sun_path) - 1] = '\0';
-
-	unlink(path.c_str()); // Remove any existing socket file
-
-	if (bind(server_fd, reinterpret_cast<struct sockaddr*>(&address), sizeof(address)) < 0) {
-		std::cerr << "Error binding Unix socket" << std::endl;
-		close(server_fd);
-		return -1;
-	}
-
-	if (listen(server_fd, 3) < 0) {
-		std::cerr << "Error listening on Unix socket" << std::endl;
-		close(server_fd);
-		return -1;
-	}
-
-	return server_fd;
-}
+BashppServer server;
+int client_fd = -1;
+int server_fd = -1;
+int socket_port = 0;
 
 int main(int argc, char* argv[]) {
 	// Trap signals
@@ -118,19 +46,15 @@ int main(int argc, char* argv[]) {
 		}
 	}
 	server.log("Command line arguments: ", args);
-	
-	client_fd = -1;
-	server_fd = -1;
-	socket_port = 0;
 
 	constexpr const char* help_string = "Bash++ Language Server " bpp_compiler_version "\n"
 		"Usage: bpp-lsp [options]\n"
 		"Options:\n"
-		"  -h, --help              Show this help message\n"
-		"  -v, --version           Show version information\n"
-		"      --stdio             Use standard input/output for communication\n"
-		"      --socket <port>     Use TCP socket for communication\n"
-		"      --pipe <path>       Use Unix domain socket for communication\n";
+		"  -h, --help          Show this help message\n"
+		"  -v, --version       Show version information\n"
+		"      --stdio         Use standard input/output for communication (default)\n"
+		"      --socket <port> Use TCP socket for communication\n"
+		"      --pipe <path>   Use Unix domain socket for communication\n";
 
 	constexpr const char* version_string = "Bash++ Language Server " bpp_compiler_version "\n"
 		"Copyright (C) 2024-" bpp_compiler_updated_year " Andrew S. Rightenburg\n"

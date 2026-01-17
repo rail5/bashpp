6ed04d836c14ac86ca3f99fc49677dd84b94bd6a
rail5 <andrew@rail5.org>
2026-01-17 14:18:25 +0800

    Listener handlers adjusting to the new AST structure

--- src/listener/handlers/subshell.cpp
+++ src/listener/handlers/subshell.cpp
@@ -5,13 +5,13 @@
 
 #include "../BashppListener.h"
 
-void BashppListener::enterSubshell(BashppParser::SubshellContext *ctx) {
+void BashppListener::enterSubshellSubstitution(std::shared_ptr<AST::SubshellSubstitution> node) {
 	skip_syntax_errors
 	// Get the current code entity
 	std::shared_ptr<bpp::bpp_code_entity> code_entity = std::dynamic_pointer_cast<bpp::bpp_code_entity>(entity_stack.top());
 
 	if (code_entity == nullptr) {
-		throw_syntax_error(ctx->SUBSHELL_START(), "Subshell outside of code entity");
+		throw_syntax_error(node, "Subshell substitution outside of code entity");
 	}
 
 	// Create a new code entity for the subshell
@@ -24,17 +24,17 @@ void BashppListener::enterSubshell(BashppParser::SubshellContext *ctx) {
 
 	subshell_entity->set_definition_position(
 		source_file,
-		ctx->SUBSHELL_START()->getSymbol()->getLine() - 1,
-		ctx->SUBSHELL_START()->getSymbol()->getCharPositionInLine()
+		node->getLine() - 1,
+		node->getCharPositionInLine()
 	);
 }
 
-void BashppListener::exitSubshell(BashppParser::SubshellContext *ctx) {
+void BashppListener::exitSubshellSubstitution(std::shared_ptr<AST::SubshellSubstitution> node) {
 	skip_syntax_errors
 	std::shared_ptr<bpp::bpp_string> subshell_entity = std::dynamic_pointer_cast<bpp::bpp_string>(entity_stack.top());
 
 	if (subshell_entity == nullptr) {
-		throw internal_error("Subshell context was not found in the entity stack", ctx);
+		throw internal_error("Subshell substitution context was not found in the entity stack");
 	}
 
 	entity_stack.pop();
@@ -43,14 +43,64 @@ void BashppListener::exitSubshell(BashppParser::SubshellContext *ctx) {
 
 	current_code_entity->add_code_to_previous_line(subshell_entity->get_pre_code());
 	current_code_entity->add_code_to_next_line("\n" + subshell_entity->get_post_code());
-	current_code_entity->add_code(ctx->SUBSHELL_START()->getText() + subshell_entity->get_code() + ctx->SUBSHELL_END()->getText());
+	current_code_entity->add_code("$(" + subshell_entity->get_code() + ")");
 
 	program->mark_entity(
 		source_file,
 		subshell_entity->get_initial_definition().line,
 		subshell_entity->get_initial_definition().column,
-		ctx->SUBSHELL_END()->getSymbol()->getLine() - 1,
-		ctx->SUBSHELL_END()->getSymbol()->getCharPositionInLine(),
+		node->getEndPosition().line - 1,
+		node->getEndPosition().column,
+		subshell_entity
+	);
+}
+
+void BashppListener::enterRawSubshell(std::shared_ptr<AST::RawSubshell> node) {
+	skip_syntax_errors
+	// Get the current code entity
+	std::shared_ptr<bpp::bpp_code_entity> code_entity = std::dynamic_pointer_cast<bpp::bpp_code_entity>(entity_stack.top());
+
+	if (code_entity == nullptr) {
+		throw_syntax_error(node, "Subshell outside of code entity");
+	}
+
+	// Create a new code entity for the subshell
+	std::shared_ptr<bpp::bpp_string> subshell_entity = std::make_shared<bpp::bpp_string>();
+	subshell_entity->set_containing_class(code_entity->get_containing_class());
+	subshell_entity->inherit(code_entity);
+
+	// Push the subshell entity onto the entity stack
+	entity_stack.push(subshell_entity);
+
+	subshell_entity->set_definition_position(
+		source_file,
+		node->getLine() - 1,
+		node->getCharPositionInLine()
+	);
+}
+
+void BashppListener::exitRawSubshell(std::shared_ptr<AST::RawSubshell> node) {
+	skip_syntax_errors
+	std::shared_ptr<bpp::bpp_string> subshell_entity = std::dynamic_pointer_cast<bpp::bpp_string>(entity_stack.top());
+
+	if (subshell_entity == nullptr) {
+		throw internal_error("Subshell context was not found in the entity stack");
+	}
+
+	entity_stack.pop();
+
+	std::shared_ptr<bpp::bpp_code_entity> current_code_entity = std::dynamic_pointer_cast<bpp::bpp_code_entity>(entity_stack.top());
+
+	current_code_entity->add_code_to_previous_line(subshell_entity->get_pre_code());
+	current_code_entity->add_code_to_next_line("\n" + subshell_entity->get_post_code());
+	current_code_entity->add_code("(" + subshell_entity->get_code() + ")");
+
+	program->mark_entity(
+		source_file,
+		subshell_entity->get_initial_definition().line,
+		subshell_entity->get_initial_definition().column,
+		node->getEndPosition().line - 1,
+		node->getEndPosition().column,
 		subshell_entity
 	);
 }

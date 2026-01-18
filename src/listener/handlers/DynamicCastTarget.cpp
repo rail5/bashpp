f81b6f4dd4456c33e915e4436c400f61bf76cd6e
rail5 <andrew@rail5.org>
2026-01-18 12:18:16 +0800

    DynamicCastTarget handler set to use new AST structure

--- src/listener/handlers/DynamicCastTarget.cpp
+++ src/listener/handlers/DynamicCastTarget.cpp
@@ -5,7 +5,7 @@
 
 #include "../BashppListener.h"
 
-void BashppListener::enterDynamic_cast_target(BashppParser::Dynamic_cast_targetContext *ctx) {
+void BashppListener::enterDynamicCastTarget(std::shared_ptr<AST::DynamicCastTarget> node) {
 	skip_syntax_errors
 	/**
 	 * Dynamic cast targets are the part of a dynamic cast statement that specifies the class we're casting to
@@ -15,7 +15,7 @@ void BashppListener::enterDynamic_cast_target(BashppParser::Dynamic_cast_targetC
 
 	std::shared_ptr<bpp::bpp_dynamic_cast_statement> dynamic_cast_entity = std::dynamic_pointer_cast<bpp::bpp_dynamic_cast_statement>(entity_stack.top());
 	if (dynamic_cast_entity == nullptr) {
-		throw internal_error("Dynamic cast context was not found in the entity stack", ctx);
+		throw internal_error("Dynamic cast context was not found in the entity stack");
 	}
 
 	std::shared_ptr<bpp::bpp_string> cast_target_entity = std::make_shared<bpp::bpp_string>();
@@ -24,42 +24,38 @@ void BashppListener::enterDynamic_cast_target(BashppParser::Dynamic_cast_targetC
 	entity_stack.push(cast_target_entity);
 }
 
-void BashppListener::exitDynamic_cast_target(BashppParser::Dynamic_cast_targetContext *ctx) {
+void BashppListener::exitDynamicCastTarget(std::shared_ptr<AST::DynamicCastTarget> node) {
 	skip_syntax_errors
 	std::shared_ptr<bpp::bpp_string> cast_target_entity = std::dynamic_pointer_cast<bpp::bpp_string>(entity_stack.top());
 	entity_stack.pop();
 	if (cast_target_entity == nullptr) {
-		throw internal_error("Dynamic cast target context was not found in the entity stack", ctx);
+		throw internal_error("Dynamic cast target context was not found in the entity stack");
 	}
 
 	std::shared_ptr<bpp::bpp_dynamic_cast_statement> dynamic_cast_entity = std::dynamic_pointer_cast<bpp::bpp_dynamic_cast_statement>(entity_stack.top());
 	if (dynamic_cast_entity == nullptr) {
-		throw internal_error("Dynamic cast context was not found in the entity stack", ctx);
+		throw internal_error("Dynamic cast context was not found in the entity stack");
 	}
 
 	// Which kind of input did we receive?
-	if (ctx->IDENTIFIER() != nullptr) {
+	if (node->TARGETTYPE().has_value()) {
 		// We have an identifier, so this is a class name
-		std::string class_name = ctx->IDENTIFIER()->getText();
+		std::string class_name = node->TARGETTYPE().value().getValue();
 		dynamic_cast_entity->set_cast_to(class_name);
 
 		// Check if the class exists
 		std::shared_ptr<bpp::bpp_class> cast_class = dynamic_cast_entity->get_class(class_name);
 		if (cast_class == nullptr) {
-			show_warning(ctx->IDENTIFIER(), "Class not found: " + class_name + ". This cast may fail at runtime.");
+			show_warning(node->TARGETTYPE().value(), "Class not found: " + class_name + ". This cast may fail at runtime.");
 		} else {
 			cast_class->add_reference(
 				source_file,
-				ctx->IDENTIFIER()->getSymbol()->getLine() - 1,
-				ctx->IDENTIFIER()->getSymbol()->getCharPositionInLine()
+				node->TARGETTYPE().value().getLine() - 1,
+				node->TARGETTYPE().value().getCharPositionInLine()
 			);
 		}
-	} else if (ctx->BASH_VAR() != nullptr) {
-		// The cast-to class is stored in a shell variable
-		// Which will have to be evaluated at runtime
-		dynamic_cast_entity->set_cast_to(ctx->BASH_VAR()->getText());
 	} else {
-		// There was an intervening object reference, which was handled by its respective parser rule,
+		// There was an intervening object reference or shell variable, which was handled by its respective parser rule,
 		// And the resolved reference will have been added to this target entity via ->add_code()
 		dynamic_cast_entity->set_cast_to(cast_target_entity->get_code());
 		dynamic_cast_entity->add_code_to_previous_line(cast_target_entity->get_pre_code());

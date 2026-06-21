05e262436660569e6497ee519eab6927626fb367
rail5 <andrew@rail5.org>
2026-06-21 12:31:19 +0800

    IR: Object structure refactor
    1. Optional initial values of objects are now stored as pointers to other
       entities, which resolve to (or generate) those values, rather than as
       std::strings -- a format which would've been appropriate to the earlier
       IR structure, where we stored compiled code
    2. Updated Object constructor to not require type/pointer info, we can set
       that later
    3. Updated pretty-printing of Object entities to account for these changes
    
    [notest]

--- src/IR/entities/Object.h
+++ src/IR/entities/Object.h
@@ -9,6 +9,7 @@
 #include <IR/bpp.h>
 #include <IR/entities/Entity.h>
 #include <IR/entities/Class.h>
+#include <IR/entities/CodeEntity.h>
 
 #include <optional>
 
@@ -28,23 +29,23 @@ class Object : public Entity {
 		std::weak_ptr<Class> type;
 
 		// Initialization information:
-		/// If a pointer, the string value to which this is initialized (if any)
-		std::optional<std::string> initial_value = std::nullopt;
+		/// If a pointer, the initial value of the pointer (if any)
+		std::optional<std::shared_ptr<CodeEntity>> initial_value = std::nullopt;
 
 		/// If not a pointer, the object from which this is copied (if any)
 		std::shared_ptr<Object> copy_from = nullptr;
 	public:
-		Object() = delete;
-		Object(std::weak_ptr<Class> type, bool is_pointer) : m_is_pointer(is_pointer), type(type) {}
-
 		const std::string& get_name() const { return name; }
 		void set_name(const std::string& name) { this->name = name; }
 
 		bool is_pointer() const { return m_is_pointer; }
-		std::weak_ptr<Class> get_type() const { return type; }
+		void set_is_pointer(bool is_pointer) { m_is_pointer = is_pointer; }
 
-		void set_initial_value(const std::string& value) { initial_value = value; }
-		const std::optional<std::string>& get_initial_value() const { return initial_value; }
+		std::weak_ptr<Class> get_type() const { return type; }
+		void set_type(std::weak_ptr<Class> type) { this->type = type; }
+
+		void set_initial_value(const std::shared_ptr<CodeEntity>& value) { initial_value = value; }
+		const std::optional<std::shared_ptr<CodeEntity>>& get_initial_value() const { return initial_value; }
 		bool has_initial_value() const { return initial_value.has_value(); }
 
 		void set_copy_from(std::shared_ptr<Object> other) { copy_from = other; }
@@ -52,16 +53,22 @@ class Object : public Entity {
 		
 		std::ostream& prettyPrint(std::ostream& os, size_t indentation_level = 0) const override {
 			std::string indent(indentation_level * 4, ' ');
-			os << indent << "(Object: " << name
-				<< ", type: " << (type.expired() ? "null" : type.lock()->get_name())
-				<< ", is_pointer: " << (m_is_pointer ? "true" : "false");
+			os << indent << "(";
+			if (type.expired()) {
+				os << "Primitive";
+			} else {
+				os << type.lock()->get_name();
+			}
+			if (m_is_pointer) os << "*";
+			os << " " << name;
 			if (initial_value.has_value()) {
-				os << ", initial_value: \"" << initial_value.value() << "\"";
+				os << "\n" << indent << "  =\n";
+				initial_value.value()->prettyPrint(os, indentation_level + 1);
 			}
 			if (copy_from != nullptr) {
-				os << ", copy_from: " << copy_from->get_name();
+				os << "\n" << indent << "  = copy of " << copy_from->get_name() << "\n";
 			}
-			os << ")\n";
+			os <<")\n";
 			return os;
 		}
 };

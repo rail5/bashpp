70281c7613ae76730bb5af19c3f369dadf551d74
rail5 <andrew@rail5.org>
2026-06-21 12:29:23 +0800

    IR: Update pretty-printing for Methods
    [notest]

--- src/IR/entities/Method.cpp
+++ src/IR/entities/Method.cpp
@@ -21,17 +21,16 @@ bool Method::add_parameter(std::shared_ptr<Object> parameter) {
 
 std::ostream& Method::prettyPrint(std::ostream& os, size_t indentation_level) const {
 	std::string indent(indentation_level * 4, ' ');
-	os << indent << "(Method: " << name;
+	os << indent << "(Method: " << name << " [";
 	switch (scope) {
-		case VisibilityScope::INACCESSIBLE: os << ", scope: INACCESSIBLE"; break;
-		case VisibilityScope::PUBLIC: os << ", scope: PUBLIC"; break;
-		case VisibilityScope::PRIVATE: os << ", scope: PRIVATE"; break;
-		case VisibilityScope::PROTECTED: os << ", scope: PROTECTED"; break;
+		case VisibilityScope::INACCESSIBLE: os << "inaccessible"; break;
+		case VisibilityScope::PUBLIC: os << "public"; break;
+		case VisibilityScope::PRIVATE: os << "private"; break;
+		case VisibilityScope::PROTECTED: os << "protected"; break;
 	}
-	os << ", is_virtual: " << (m_is_virtual ? "true" : "false")
-		<< ", is_overridable: " << (m_is_overridable ? "true" : "false")
-		<< ", is_inherited: " << (m_is_inherited ? "true" : "false")
-		<< "\n";
+	if (m_is_virtual) os << ", virtual";
+	if (m_is_inherited) os << ", inherited";
+	os << "]\n";
 	for (const auto& param : parameters) {
 		param->prettyPrint(os, indentation_level + 1);
 	}

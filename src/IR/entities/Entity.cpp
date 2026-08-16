d1d169b0bfc83697e0ef8b46dd54c679471caa24
rail5 <andrew@rail5.org>
2026-08-17 00:12:35 +0800

    Improve assertion message for NamedEntities
    [notest]

--- src/IR/entities/Entity.cpp
+++ src/IR/entities/Entity.cpp
@@ -31,7 +31,12 @@ void Entity::inherit(std::shared_ptr<Entity> parent) {
 }
 
 std::shared_ptr<Class> Entity::get_class(const std::string& name, std::size_t /*max_visible_index*/) const {
-	bpp_assert(!containing_program.expired(), std::string("Entity does not have a containing program"));
+	bpp_assert(!containing_program.expired(),
+		std::string("Entity")
+			+ (dynamic_cast<const NamedEntity*>(this)
+				? std::string(" '" + dynamic_cast<const NamedEntity*>(this)->get_name() + "'")
+				: std::string(""))
+			+ std::string(" does not have a containing program"));
 	const std::shared_ptr<Program> containing_program_ptr = containing_program.lock();
 	return containing_program_ptr->get_class(name, program_visible_class_count_at_creation);
 }

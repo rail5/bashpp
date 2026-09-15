19918741342dab43ca68de9ecc55257e3c3db4b8
rail5 <andrew@rail5.org>
2026-09-15 15:30:09 +0800

    IR::ValueAssignment: Change rvalue_object type to ObjectReference entity
    [notest]

--- src/IR/entities/expressions/ValueAssignment.h
+++ src/IR/entities/expressions/ValueAssignment.h
@@ -14,21 +14,19 @@ namespace bpp::IR {
 class ValueAssignment : public StringType {
 	private:
 		bool lvalue_nonprimitive = false;
-		bool rvalue_nonprimitive = false;
-		std::shared_ptr<Object> rvalue_object = nullptr;
+		std::shared_ptr<ObjectReference> rvalue_object = nullptr;
 
 		bool array_assignment = false; // E.g arr=()
 		bool adding = false; // E.g. arr+=("value")
 	public:
 		void setLvalueNonprimitive(bool is_nonprimitive) { lvalue_nonprimitive = is_nonprimitive; }
-		void setRvalueNonprimitive(bool is_nonprimitive) { rvalue_nonprimitive = is_nonprimitive; }
-		void setRvalueObject(std::shared_ptr<Object> obj) { rvalue_object = std::move(obj); }
+		void setRvalueObject(std::shared_ptr<ObjectReference> obj) { rvalue_object = std::move(obj); }
 		void setArrayAssignment(bool is_array_assignment) { array_assignment = is_array_assignment; }
 		void setAdding(bool is_adding) { adding = is_adding; }
 
 		bool isLvalueNonprimitive() const { return lvalue_nonprimitive; }
-		bool isRvalueNonprimitive() const { return rvalue_nonprimitive; }
-		std::shared_ptr<Object> getRvalueObject() const { return rvalue_object; }
+		bool isRvalueNonprimitive() const { return rvalue_object != nullptr; }
+		std::shared_ptr<ObjectReference> getRvalueObject() const { return rvalue_object; }
 		bool isArrayAssignment() const { return array_assignment; }
 		bool isAdding() const { return adding; }
 
